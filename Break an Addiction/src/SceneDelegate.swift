78596d4569070ca7d9f8dd0a62a06a17df683c37
Nihad Samedov <the_nihad@icloud.com>
2021-11-04 14:34:06 +0300

    Refactor

---
+++ Break an Addiction/src/SceneDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  SceneDelegate.swift
+//  Break an Addiction
+//
+//  Created by Nihad on 12/11/20.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let scene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: scene)
+        window?.rootViewController = RootIntroPageViewController()
+//        window?.rootViewController = MainTabController()
+        window?.makeKeyAndVisible()
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+    }
+}
+
